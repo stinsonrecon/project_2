ad26d6e314d3022abd635e1b30aa689114bb3e29
Lê Minh Tiến <minhtienlomo@gmail.com>
2023-08-24 11:44:52 +0700

    fix(admin): fix bug estate

--- resources/views/admin/content/house/index.blade.php
+++ resources/views/admin/content/house/index.blade.php
@@ -15,88 +15,91 @@
         <div class=" h-screen">
             <div class="mt-2">
                 <div class="overflow-x-auto flex flex-col">
-                    <div class="flex flex-row-reverse">
-                        <div class="m-6">
-                            <a href="{{route('admin.house.create')}}"
-                                class="btn focus:outline-none focus:shadow-outline"
-                                type="button">
-                                Thêm bất động sản
-                            </a>
-                        </div>
-                    </div>
-                <div class=" w-11/12  mx-auto mx-8 bg-green-700  table-auto">
-                    @if(session()->has('success'))
-
-                        <div class="bg-green-300 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
-                            <div class="flex">
-                            <div class="py-1"><i class=" fas fa-check-circle fill-current h-6 w-6 text-green-700 mr-4"> </i></div>
-
-                            <div>
-
-                                <p class="text-lg">{{ session()->get('success') }}</p>
-                            </div>
+                    @can('create estate')
+                        <div class="flex flex-row-reverse">
+                            <div class="m-6">
+                                <a href="{{route('admin.house.create')}}"
+                                    class="btn focus:outline-none focus:shadow-outline"
+                                    type="button">
+                                    Thêm bất động sản
+                                </a>
                             </div>
                         </div>
-                    @endif
-                </div>
-                <div>
-                    <div class=" w-11/12  mx-auto mx-8  table-auto pb-5">
-                        {{ $houses->links() }}
+                    @endcan
+                    <div class=" w-11/12  mx-auto mx-8 bg-green-700  table-auto">
+                        @if(session()->has('success'))
+
+                            <div class="bg-green-300 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                                <div class="flex">
+                                <div class="py-1"><i class=" fas fa-check-circle fill-current h-6 w-6 text-green-700 mr-4"> </i></div>
+
+                                <div>
+
+                                    <p class="text-lg">{{ session()->get('success') }}</p>
+                                </div>
+                                </div>
+                            </div>
+                        @endif
                     </div>
-                    <table class=" w-11/12  mx-auto mx-8  table-auto">
-                        <thead>
-                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                                <th class="py-3 px-6 text-center">Mã tin</th>
-                                <th class="py-3 px-6 text-center">Địa chỉ</th>
-                                <th class="py-3 px-6 text-center">Diện tích</th>
-                                <th class="py-3 px-6 text-center">Pháp lý</th>
-                                <th class="py-3 px-6 text-center">Hành động</th>
+                    <div>
+                        <div class=" w-11/12  mx-auto mx-8  table-auto pb-5">
+                            {{ $houses->links() }}
+                        </div>
+                        <table class=" w-11/12  mx-auto mx-8  table-auto">
+                            <thead>
+                                <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                    <th class="py-3 px-6 text-center">Mã tin</th>
+                                    <th class="py-3 px-6 text-center">Địa chỉ</th>
+                                    <th class="py-3 px-6 text-center">Diện tích</th>
+                                    <th class="py-3 px-6 text-center">Pháp lý</th>
+                                    <th class="py-3 px-6 text-center">Hành động</th>
 
-                            </tr>
-                        </thead>
-                        <tbody class="text-sm font-light">
-                            @foreach($houses as $house)
-                            <tr class="border-b border-gray-200 hover:bg-gray-100 hover:text-black">
-                                <td class="py-3 px-6 text-left whitespace-nowrap">
-                                    <a href="{{route('admin.house.detail',['id' => $house->id])}}" class="align-middle hover:text-gray-400 border-b-2 border-gray-800">
-                                        <span class="font-medium">{{ $house->news->idBanking }}</span>
-                                    </a>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <span class="font-medium">
-                                        {{ $house->city->name }},
-                                        {{ $house->district->name }},
-                                        @if($house->xaid != null)
-                                            {{ $house->ward->name }},
-                                        @endif
-                                        {{ $house->ten_duong }}
-                                    </span>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <span class="font-medium">{{ $house->dientich }} m²</span>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <span class="font-medium">{{ $house->phap_ly }}</span>
-                                </td>
-                                <td class="py-3 px-6 text-left">
-                                    <div class="flex item-center justify-center">
+                                </tr>
+                            </thead>
+                            <tbody class="text-sm font-light">
+                                @foreach($houses as $house)
+                                <tr class="border-b border-gray-200 hover:bg-gray-100 hover:text-black">
+                                    <td class="py-3 px-6 text-left whitespace-nowrap">
+                                        <a href="{{route('admin.house.detail',['id' => $house->id])}}" class="align-middle hover:text-gray-400 border-b-2 border-gray-800">
+                                            <span class="font-medium">{{ $house->news->idBanking }}</span>
+                                        </a>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <span class="font-medium">
+                                            {{ $house->city->name }},
+                                            {{ $house->district->name }},
+                                            @if($house->xaid != null)
+                                                {{ $house->ward->name }},
+                                            @endif
+                                            {{ $house->ten_duong }}
+                                        </span>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <span class="font-medium">{{ $house->dientich }} m²</span>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <span class="font-medium">{{ $house->phap_ly }}</span>
+                                    </td>
+                                    <td class="py-3 px-6 text-left">
+                                        <div class="flex item-center justify-center">
 
-                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <a href="{{ route('admin.house.edit', ['id' => $house->id]) }}" class="">
-                                                <i class="fas fa-edit"></i>
-                                            </a>
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a href="{{ route('admin.house.edit', ['id' => $house->id]) }}" class="">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                            </div>
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a class="" onclick="return myFunction();" href="{{ route('admin.house.delete', ['id' => $house->id]) }}" >
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </a>
+                                            </div>
                                         </div>
-                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <a class="" onclick="return myFunction();" href="{{ route('admin.house.delete', ['id' => $house->id]) }}" >
-                                                <i class="fas fa-trash-alt"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/admin/content/land/index.blade.php
+++ resources/views/admin/content/land/index.blade.php
@@ -15,88 +15,91 @@
         <div class=" h-screen">
             <div class="mt-2">
                 <div class="overflow-x-auto flex flex-col">
-                    <div class="flex flex-row-reverse">
-                        <div class="m-6">
-                            <a href="{{route('admin.land.create')}}"
-                                class="btn focus:outline-none focus:shadow-outline"
-                                type="button">
-                                Thêm bất động sản
-                            </a>
-                        </div>
-                    </div>
-                <div class=" w-11/12  mx-auto mx-8 bg-green-700  table-auto">
-                    @if(session()->has('success'))
-
-                        <div class="bg-green-300 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
-                            <div class="flex">
-                            <div class="py-1"><i class=" fas fa-check-circle fill-current h-6 w-6 text-green-700 mr-4"> </i></div>
-
-                            <div>
-
-                                <p class="text-lg">{{ session()->get('success') }}</p>
-                            </div>
+                    @can('create estate')
+                        <div class="flex flex-row-reverse">
+                            <div class="m-6">
+                                <a href="{{route('admin.land.create')}}"
+                                    class="btn focus:outline-none focus:shadow-outline"
+                                    type="button">
+                                    Thêm bất động sản
+                                </a>
                             </div>
                         </div>
-                    @endif
-                </div>
-                <div>
-                    <div class=" w-11/12  mx-auto mx-8  table-auto pb-5">
-                        {{ $lands->links() }}
+                    @endcan
+                    <div class=" w-11/12  mx-auto mx-8 bg-green-700  table-auto">
+                        @if(session()->has('success'))
+
+                            <div class="bg-green-300 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                                <div class="flex">
+                                <div class="py-1"><i class=" fas fa-check-circle fill-current h-6 w-6 text-green-700 mr-4"> </i></div>
+
+                                <div>
+
+                                    <p class="text-lg">{{ session()->get('success') }}</p>
+                                </div>
+                                </div>
+                            </div>
+                        @endif
                     </div>
-                    <table class=" w-11/12  mx-auto mx-8  table-auto">
-                        <thead>
-                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                                <th class="py-3 px-6 text-center">Mã tin</th>
-                                <th class="py-3 px-6 text-center">Địa chỉ</th>
-                                <th class="py-3 px-6 text-center">Diện tích</th>
-                                <th class="py-3 px-6 text-center">Pháp lý</th>
-                                <th class="py-3 px-6 text-center">Hành động</th>
+                    <div>
+                        <div class=" w-11/12  mx-auto mx-8  table-auto pb-5">
+                            {{ $lands->links() }}
+                        </div>
+                        <table class=" w-11/12  mx-auto mx-8  table-auto">
+                            <thead>
+                                <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                    <th class="py-3 px-6 text-center">Mã tin</th>
+                                    <th class="py-3 px-6 text-center">Địa chỉ</th>
+                                    <th class="py-3 px-6 text-center">Diện tích</th>
+                                    <th class="py-3 px-6 text-center">Pháp lý</th>
+                                    <th class="py-3 px-6 text-center">Hành động</th>
 
-                            </tr>
-                        </thead>
-                        <tbody class="text-sm font-light">
-                            @foreach($lands as $land)
-                            <tr class="border-b border-gray-200 hover:bg-gray-100 hover:text-black">
-                                <td class="py-3 px-6 text-left whitespace-nowrap">
-                                    <a href="{{route('admin.land.detail',['id' => $land->id])}}" class="align-middle hover:text-gray-400 border-b-2 border-gray-800">
-                                        <span class="font-medium">{{ $land->news->idBanking }}</span>
-                                    </a>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <span class="font-medium">
-                                        {{ $land->city->name }},
-                                        {{ $land->district->name }},
-                                        @if($land->xaid != null)
-                                            {{ $land->ward->name }},
-                                        @endif
-                                        {{ $land->ten_duong }}
-                                    </span>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <span class="font-medium">{{ $land->dientich }} m²</span>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <span class="font-medium">{{ $land->phap_ly }}</span>
-                                </td>
-                                <td class="py-3 px-6 text-left">
-                                    <div class="flex item-center justify-center">
+                                </tr>
+                            </thead>
+                            <tbody class="text-sm font-light">
+                                @foreach($lands as $land)
+                                <tr class="border-b border-gray-200 hover:bg-gray-100 hover:text-black">
+                                    <td class="py-3 px-6 text-left whitespace-nowrap">
+                                        <a href="{{route('admin.land.detail',['id' => $land->id])}}" class="align-middle hover:text-gray-400 border-b-2 border-gray-800">
+                                            <span class="font-medium">{{ $land->news->idBanking }}</span>
+                                        </a>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <span class="font-medium">
+                                            {{ $land->city->name }},
+                                            {{ $land->district->name }},
+                                            @if($land->xaid != null)
+                                                {{ $land->ward->name }},
+                                            @endif
+                                            {{ $land->ten_duong }}
+                                        </span>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <span class="font-medium">{{ $land->dientich }} m²</span>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <span class="font-medium">{{ $land->phap_ly }}</span>
+                                    </td>
+                                    <td class="py-3 px-6 text-left">
+                                        <div class="flex item-center justify-center">
 
-                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <a href="{{ route('admin.land.edit', ['id' => $land->id]) }}" class="">
-                                                <i class="fas fa-edit"></i>
-                                            </a>
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a href="{{ route('admin.land.edit', ['id' => $land->id]) }}" class="">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                            </div>
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a class="" onclick="return myFunction();" href="{{ route('admin.land.delete', ['id' => $land->id]) }}" >
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </a>
+                                            </div>
                                         </div>
-                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <a class="" onclick="return myFunction();" href="{{ route('admin.land.delete', ['id' => $land->id]) }}" >
-                                                <i class="fas fa-trash-alt"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
